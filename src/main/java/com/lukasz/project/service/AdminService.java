8af33dd582422ad1e6799307f5f563b1d1d020c8
iamruppert <lavishlawyer@tutanota.com>
2023-12-01 18:23:41 +0100

    define services

---
+++ src/main/java/com/lukasz/project/service/AdminService.java
@@ -0,0 +1,36 @@
+package com.lukasz.project.service;
+
+import com.lukasz.project.entity.Admin;
+import com.lukasz.project.entity.Role;
+import com.lukasz.project.repository.AdminRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+
+
+    private final AdminRepository adminRepository;
+
+    public void createAdmin(Admin admin){
+        // Dodajemy logikę sprawdzania, czy rekruter o podanym emailu już istnieje
+        if (adminRepository.existsByEmail(admin.getEmail())) {
+            throw new RuntimeException("Admin with this email already exists");
+        }
+
+        admin.setRole(Role.ADMIN);
+
+        adminRepository.save(admin);
+    }
+
+
+    public List<Admin> findAllAdmin(){
+        return adminRepository.findAll();
+    }
+
+
+
+}
